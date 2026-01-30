f69f4ce4cba860039a7445b552dc3de8ef314e0b
RAJ KUMAR <rajkumarp29>
2026-01-30 22:44:30 +0530

    Sprint 1: Initial project setup

---
+++ backends/src/main/java/com/portfolio/BackendsApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendsApplication.class, args);
+	}
+
+}
